84ff45ffd4558938c8b781305656341f2f4e4ef3
jafe sellers <jafesellers@gmail.com>
2017-11-29 17:47:07 -0500

    start adding blank classes for base game mechanics

---
+++ src/chess/Board.py
@@ -0,0 +1,5 @@
+# defines a game board and handles piece movement initalizes to a new game state
+class Board:
+    #constructor for board class
+    def __init__(self):
+        pass
---
+++ src/chess/Pieces/Pawn.py
@@ -0,0 +1,4 @@
+# defines a Pawn object contains methods for moving a pawn used by Board.py
+class Pawn:
+    def __init__(self):
+        pass
