379b48c99e68dc7f3f182363671a0bac1972e27c
Daniel Darnell <danieldarnell8188@yahoo.com>
2017-12-01 11:08:15 -0500

    moved board to src

---
+++ src/board/Board.py
@@ -0,0 +1,8 @@
+""" TODO Create board for engine """
+
+""" Currently there are two board implementations in development branches.
+These need to be merged. Until then, master should contain an api that will
+likely be implmented in the future. """
+
+class Board():
+    pass
