9666c9b63162499c8c2fab3e55663cbb5e9febe5
Daniel Darnell <danieldarnell8188@yahoo.com>
2017-12-01 13:59:04 -0500

    created collab notes
    added notes to board.py

--- src/Board.py
+++ src/Board.py
@@ -1,21 +1,46 @@
+""" General code style:
+    TODO line vs triple quote comments?
+
+    python doc?
+
+    spacing between lines?
+
+        Good for vim
+
+    use of whitespace in general
+
+    correct spelling errors
+"""
+
 # defines a game board and handles piece movement initalizes to a new game state
 class Board:
     chess_board=[]
+    # TODO (daniel): comment should be descriptive of desired behavior. eg:
+    # 'print debugging message and initialize board state to starting position'
     #constructor for board class
     def __init__(self):
+        # TODO (daniel): Debugging output should be moved outside of board
+        # class
         print("Hello From Board")
         self.new_board()
         
     #checks if piece can move and then if moveable changes the location of the piece
     def movePiece(start,end):
+        # TODO (daniel) inconsistent spacing
         firstValues= "abcdefgh"
         secondValues="12345678"
+        # TODO (daniel) usually logical and is preffered, unless the intent is
+        # to use the value returned. The latter does not appear to be the case
+        # here
         if len(start)&len(end) == 2:
             if start[0]in firstValues & start[1]in secondValues:
                 if end[0]in firstValues & end[1]in secondValues:
+                    # TODO (daniel) Document unfimplemented behavior with a todo
                     # figure out if piece can move now
                     pass
 
+    # (daniel) This is a good example of correct documentation and proper
+    # implementation
     #resets bord to new game state white == upper black  ==  lower
     def new_board(self):
         self.chess_board=["RNBQKBNR",
@@ -31,6 +56,7 @@ class Board:
     #x,y should be in terms of array values
     def checkMove(self,xStart,yStart,xEnd,yEnd):
         piece=self.chess_board[xStart][yStart]
+        # TODO (daniel) bitwise is invalid here. Perhaps logical or?
         #check piece movement
         if piece == 'p' | piece == 'P':
             return checkPawn(self.chess_board,piece,xStart,yStart,xEnd,yEnd)
@@ -58,6 +84,8 @@ class Board:
 
     #pawn logic hidden here
     def checkPawn(board,piece,xStart,yStart,xEnd,yEnd):
+    # TODO (daniel) correct indentation
+    # TODO (daniel) simplifiy if statement
     #todo add En passant eventually needs check lastmove varialbe
         #black pawn logic
         if piece == 'p':
