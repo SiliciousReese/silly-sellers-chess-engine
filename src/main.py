892b9d782f5d25f41de7748f2367c75ef0315368
Daniel Darnell <danieldarnell8188@yahoo.com>
2017-12-03 15:45:44 -0500

    prevented file not found
    prevented directory not found in maim

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+
+# Check if resource folder exists
+import os
+
 # Python logging
 import logging
 
@@ -9,7 +13,15 @@ from board.Board import Board
 
 def main():
     # TODO implement better file handling.
+
     __res_file_name = "res/"
+
+    # Temporarily avoid using res folder to avoid runtime error if folder
+    # does not exist.
+    res_file_exists = os.path.exists(__res_file_name)
+    if not res_file_exists:
+        __res_file_name = "./"
+
     __log_file_name = __res_file_name + "chess-engine-log.log"
 
     # Start logger
