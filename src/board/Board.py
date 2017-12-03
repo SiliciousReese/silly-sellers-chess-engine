802e90cf299a2b8eaf3fd52f5bd7428cee046993
Daniel Darnell <danieldarnell8188@yahoo.com>
2017-12-03 00:27:28 -0500

    added make move function to board
    fixed api
    
    cleaned up formatting

--- src/board/Board.py
+++ src/board/Board.py
@@ -1,8 +1,7 @@
-""" TODO Create board for engine """
+# Currently there are two board implementations in development branches.  These
+# need to be merged. Until then, master should contain an api that will likely
+# be implmented in the future. """
 
-""" Currently there are two board implementations in development branches.
-These need to be merged. Until then, master should contain an api that will
-likely be implmented in the future. """
 
 class Board():
     """ This will act as the backend for most of the engine. After it is
@@ -16,13 +15,18 @@ class Board():
         """ TODO Ascii representation of the board. """
         return ""
 
-    def read_from_fen(fen_string):
+    def read_from_fen(self, fen_string):
         """ TODO Process a position from an fen string """
         pass
 
     def get_all_legal_moves(self):
-        """ TODO Return an array of strings representing all legal moves from the
-        current board position """
+        """ TODO Return an array of strings representing all legal moves from
+        the current board position """
+        pass
+
+    def make_move(self, move):
+        """ TODO Make a move on the board. Throw an exception if the move is
+        not valid. """
         pass
 
     def get_fen(self):
